f63526aedc746a69be91bc39e315f1f7cb1bb02f
Raunaq Gupta <raunaq.rg@gmail.com>
2013-10-13 15:53:51 +0100

    Switch for home and menu cards

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,9 @@
+$(document).ready(function(){
+  $("#ctrl_menu").on("click", function(e){
+    $("#location").css({"display": "none"});
+  });
 
+  $("#ctrl_home").on("click", function(e){
+    $("#location").css({"display": "inline-block"});
+  });
+});
