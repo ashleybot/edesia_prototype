7740ca7fec5830863456efacf4b1c35a3ba7fe39
Raunaq Gupta <raunaq.rg@gmail.com>
2013-10-14 15:23:53 +0100

    Added menu skeleton

--- scripts/main.js
+++ scripts/main.js
@@ -6,4 +6,21 @@ $(document).ready(function(){
   $("#ctrl_home").on("click", function(e){
     $("#location").css({"display": "inline-block"});
   });
+
+  //menu controls
+  $("#appetizers").on("click", function(){
+    $(".menu_content > .bg_img").css({
+      "background-image": 'url("./static/images/menu/fish.jpg")'
+    });
+  });
+  $("#soups").on("click", function(){
+    $(".menu_content > .bg_img").css({
+      "background-image": "url('./static/images/menu/egg.jpg')"
+    });
+  });
+  $("#dishes").on("click", function(){
+    $(".menu_content > .bg_img").css({
+      "background-image": "url('./static/images/menu/fruits.jpg')"
+    });
+  });
 });
