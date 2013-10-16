8b52f436d4a6f6970ff3d954c2e85258892e443b
Raunaq Gupta <raunaq.rg@gmail.com>
2013-10-16 16:49:19 +0100

    Static image panorama mouse interaction

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,22 @@
+window.onload = function(){
 
+	var mid_width = $("body").width()/2;
+	console.log(mid_width);
+	$("#hairline").on("click", function(e){
+		$("#title_overlay").toggleClass("hide");
+		$(this).toggleClass("hide");
+	});
+
+	$(window).on("mousemove", function(e){
+		var diff = event.clientX - mid_width;
+		var rate = (10/mid_width)*diff;
+		var current_bgpx = parseFloat($("body").css("background-position-x"));
+    console.log(rate);
+		console.log(current_bgpx);
+    var per = (current_bgpx + rate);
+    if(per >= 0 && per <= 100)
+		$("body").css({
+		 "background-position-x": per.toString() + "%"
+		});
+	});
+}
