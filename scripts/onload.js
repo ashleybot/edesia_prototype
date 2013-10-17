55b3e08484b8d063946cd08f207a81215b9d5762
Raunaq Gupta <raunaq.rg@gmail.com>
2013-10-17 07:04:37 +0100

    Added background panoramic image

--- scripts/onload.js
+++ scripts/onload.js
@@ -11,4 +11,27 @@ window.onload = function(){
   		"opacity": $("#menu_overlay").scrollTop()/maxheight
   	});
   });
+
+  var mid_width = $("body").width()/2;
+  console.log(mid_width);
+  $("#hairline").on("click", function(e){
+          $("#map_overlay").toggleClass("hide");
+          $("#overlay").toggleClass("hide");
+          $("#menu_overlay").toggleClass("hide");
+          $(this).toggleClass("hide");
+  });
+
+  $(window).on("mousemove", function(e){
+    var diff = event.clientX - mid_width;
+    var rate = (1/mid_width)*diff;
+    var current_bgpx = parseFloat($("body").css("background-position-x"));
+    console.log(rate);
+    console.log(current_bgpx);
+    var per = (current_bgpx + rate);
+    if(per >= 0 && per <= 100){
+      $("body").css({
+       "background-position-x": per.toString() + "%"
+      });
+    }
+  });
 };
