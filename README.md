553e7321490792ec4e3d13f8ba1205b788a2a476
ashleygreer <e.a.greer@gmail.com>
2013-11-05 13:06:46 +0000

    using foundation 4. something wasn't working so I needed to blow everything away first

--- README.md
+++
@@ -1,8 +0,0 @@
-MHCI Capstone Prototype
-================
-
-To build CSS:
-
-1. Install Ruby
-2. Install Compass
-3. Run `compass watch` in a terminal
--- config.rb
+++ config.rb
@@ -1,11 +1,13 @@
+require 'zurb-foundation'
 # Require any additional compass plugins here.
+
 
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "stylesheets"
 sass_dir = "sass"
-images_dir = "static/images"
-javascripts_dir = "	scripts"
+images_dir = "images"
+javascripts_dir = "javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
@@ -14,7 +16,7 @@ javascripts_dir = "	scripts"
 # relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-line_comments = false
+# line_comments = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
