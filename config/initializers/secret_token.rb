ae3ac5e216d4f2050661797f3cdc49da1845a0ec
ashleygreer <e.a.greer@gmail.com>
2013-11-07 12:00:44 +0000

    is there a time to commit something if you know it doesn't work? Y

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EdesiaPrototype::Application.config.secret_key_base = 'cdcbe895aa6fc3c2aa67942aa81baa5d1664f9432ba4e4639d9f0bdfe02dbdcb4ffafd2f5d6dbb007f831e590ba63825176ebb4dc48ca0c8fce643303019fb42'
