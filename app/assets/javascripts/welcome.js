8f12f4699b6e52aad52f2fba3f7d017e4c18d68a
ashleygreer <e.a.greer@gmail.com>
2013-11-07 20:04:18 +0000

    some basic parallax. most importantly, it doesn't break foundation

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,2 +1,13 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).ready(function () {    
+    var portholeContainer = $('#porthole_container');
+    var pHeight = portholeContainer.outerHeight();
+    console.log(pHeight);
+    $('#porthole_container > div').each(function() {
+
+      $(this).css({ height: pHeight + 'px' });
+
+    });
+});
