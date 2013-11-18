a56daa3441b8bab3f5ece3a1fa59879ae58d7b04
ashleygreer <e.a.greer@gmail.com>
2013-11-18 12:15:37 +0000

    using new interior. Changed arrows to buttons

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -3,6 +3,13 @@
 
 $(document).ready(function () {    
   var inside = true;
+  var push_left, push_right = false;
+  
+  /*$('#outside').toggle(function() {
+    $("#outside").animate({backgroundPositionY: '-=100px'}, 4000);
+  }, function() {
+    $("#outside").animate({backgroundPositionY: '+=100px'}, 4000);
+  });*/
   
   $('#logo').click(function(){
       $('#inside').fadeIn();
@@ -11,10 +18,29 @@ $(document).ready(function () {
       inside = true;
   });
     
-    $('#porthole_container').click(function() {
-      $('#inside').fadeOut();
-      $('#ui').fadeOut();
-      //$('#logo').html('Back');
-      inside = false;
-    });
+  $('#porthole_container').click(function() {
+    $('#inside').fadeOut();
+    $('#ui').fadeOut();
+    //$('#logo').html('Back');
+    inside = false;
+  });
+  
+  $('#porthole_right').hover(function() {
+    console.log('mouse is in hover');
+  }, function() {
+    console.log('mouse out');
+  });
+  
+  $('#push_left').hover(function(){
+    push_left = true;
+  }, function(){
+    push_left = false;
+  });
+  
+  $('#push_right').hover(function(){
+    push_right = true;
+  }, function(){
+    push_right = false;
+  });
+  
 });
