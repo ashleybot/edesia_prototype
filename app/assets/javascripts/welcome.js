07f69dbf8e66aef1b756613f81744f5a54145b29
ashleygreer <e.a.greer@gmail.com>
2013-11-18 12:58:40 +0000

    removed color from buttons and positioned

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -5,33 +5,21 @@ $(document).ready(function () {
   var inside = true;
   var push_left, push_right = false;
   
-  /*$('#outside').toggle(function() {
-    $("#outside").animate({backgroundPositionY: '-=100px'}, 4000);
-  }, function() {
-    $("#outside").animate({backgroundPositionY: '+=100px'}, 4000);
-  });*/
-  
   $('#logo').click(function(){
       $('#inside').fadeIn();
       $('#ui').fadeIn();
-      //$('#logo').html('Back');
       inside = true;
   });
     
   $('#porthole_container').click(function() {
     $('#inside').fadeOut();
     $('#ui').fadeOut();
-    //$('#logo').html('Back');
     inside = false;
   });
   
-  $('#porthole_right').hover(function() {
-    console.log('mouse is in hover');
-  }, function() {
-    console.log('mouse out');
-  });
-  
   $('#push_left').hover(function(){
+    //$('#outside').animate({backgroundPositionZ: "20px"}, 500);
+    console.log("go left");
     push_left = true;
   }, function(){
     push_left = false;
@@ -39,6 +27,7 @@ $(document).ready(function () {
   
   $('#push_right').hover(function(){
     push_right = true;
+    console.log("go right");
   }, function(){
     push_right = false;
   });
