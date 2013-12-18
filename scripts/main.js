e019f79f9dbaccd78ac311be7ba0ba079ef22856
Raunaq Gupta <raunaq.rg@gmail.com>
2013-12-18 12:40:32 +0000

    Updated nav bar

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,10 @@
 App.prev_ship_imgnum = 2;
 
 window.onload = function(){
-  console.log("loaded");
-  //initialize google map
-  /*var gmap = App.GMap("map_tour");
-  gmap.addMarker(38.691584, -9.215977, "Belem Tower", "Belem Tower");
-  gmap.addMarker(38.693597, -9.205712, "Discoveries Monument", "Discoveries Monument");
-  gmap.addMarker(38.6980952, -9.1790209, "Ponte 25 Abril", "Ponte 25 Abril");
-  gmap.addMarker(38.713909, -9.133476, "Castle of Sao Jorge", "Castle of Sao Jorge");
-  gmap.addMarker(38.7078908, -9.1369315, "Praça do Comércio", "Praça do Comércio");
-  //gmap.addMarker(38.7101438, -9.1333138, "Largo Santo António da Sé", "Largo Santo António da Sé");
-  //gmap.addMarker(38.6991685, -9.2203687, "Museu da Electricidade", "Museu da Electricidade");
-  gmap.setMapCenter(38.690349, -9.189935);
-
   //initialize restaurant location map
   var location_map = App.GMap("location_map");
   location_map.setMapCenter(51.512432, -0.141823);
-  location_map.addMarker(51.512432, -0.141823, "Conduit St. ");*/
+  location_map.addMarker(51.512432, -0.141823, "Conduit St. ");
 
   // change logo for floating navbar
   $("#li_menu").hover(function(){
@@ -101,10 +89,16 @@ window.onload = function(){
     var diff = Math.floor(pagewidth / numimages);
     var img_num = Math.min(Math.floor(e.pageX/diff), 50) + 407;
     if(img_num != App.prev_ship_imgnum) {
-      if(img_num < 10) {
-        img_num = img_num.toString();
+      var counter = img_num > App.prev_ship_imgnum ? 1 : -1;
+      while(img_num - App.prev_ship_imgnum != 0) {
+        App.prev_ship_imgnum = App.prev_ship_imgnum + counter;
+        $("#ship").attr("src", "static/images/ship/final0" + App.prev_ship_imgnum + ".png");
       }
-      $("#ship").attr("src", "static/images/ship/test0" + img_num + ".png");
+
+      var move = Math.min(e.pageX - 600, 0).toString() + "px";
+      $("#youtube_video").css({
+        "left": move
+      });
     }
     App.prev_ship_imgnum = parseInt(img_num);
   });
